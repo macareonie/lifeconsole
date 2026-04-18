6198efefda44ebe75eecc4c8e9ffbf0222b775bd
macareonie <ryanchiang189@gmail.com>
2026-04-19 05:00:42 +0800

    Setup client connection with supabase project

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,1 +1,4 @@
-export const db = {};
+import { createClient } from "@supabase/supabase-js";
+import { env } from "./env.js";
+
+export const db = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,3 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const env = {
   PORT: process.env.PORT || 3000,
+  SUPABASE_URL: process.env.SUPABASE_URL || "",
+  SUPABASE_KEY: process.env.SUPABASE_KEY || "",
 };
